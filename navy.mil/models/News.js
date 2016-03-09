dc3ed6d5050cf9cb92dd600a32680c3243303e9d
Wei Chengli <foundwei@qq.com>
2016-03-09 14:55:25 +0800

    Data model for News
    ORM of News

---
+++ navy.mil/models/News.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview Model for News object.
+ *
+ * @author Wei Chengli (foundwei@qq.com)
+ * @version 1.0 (2016-03-08)
+ */
+
+var db = require('../../util/dbconnection.js');
+var Schema = db.mongoose.Schema;
+
+var NewsSchema = new Schema({
+	pubdate: String,
+	content: String
+});
+
+var News = db.mongoose.model('News', NewsSchema);
+var NewsDAO = function() {};
+
+NewsDAO.prototype.save = function(obj, callback) {
+	var instance = new News(obj);
+	instance.save(function(err) {
+		callback(err);
+	});
+}
+
+NewsDAO.prototype.findByDate = function(date, callback) {
+	News.findOne({pubdate: date}, function(err, obj) {
+		callback(err, obj);
+	});
+}
+
+module.exports = new NewsDAO();
